69d7c2a48bf43c7385a819578201b54d6bc948b2
Alex Douglas <adsjcsp@gmail.com>
2021-12-28 17:08:59 -0300

    primeira vez

---
+++ ext/db/init_db.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+def banco_de_dados():
+
+    connection = sqlite3.connect('database.db')
+
+
+    with open('schema.sql') as f:
+        connection.executescript(f.read())
+
+    cur = connection.cursor()
+
+    cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+                ('First Post', 'Content for the first post')
+                )
+
+    cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+                ('Second Post', 'Content for the second post')
+                )
+
+    connection.commit()
+    connection.close()
+    
+    return
